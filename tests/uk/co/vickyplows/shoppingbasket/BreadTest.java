4c0811c50696fb9ae397f1add3d2aa7bb1e95728
vixplows <vicky@vickyplows.co.uk>
2017-07-23 20:07:43 +0100

    Pass Bread testGetProductCode

--- tests/uk/co/vickyplows/shoppingbasket/BreadTest.java
+++ tests/uk/co/vickyplows/shoppingbasket/BreadTest.java
@@ -1,12 +1,25 @@
 package uk.co.vickyplows.shoppingbasket;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BreadTest {
+
+    private Bread bread;
+
     @Before
     public void setUp() throws Exception {
+        bread = new Bread ("a002", "Bread", 1.00, true);
     }
+
+    @Test
+    public void testGetProduceCode() {
+        String productCode = bread.getProductCode();
+        assertEquals("a002", productCode);
+    }
+
+    
 
 }
