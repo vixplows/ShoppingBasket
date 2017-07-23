989f6d326ab8a6522bf3d00a9159655d4ac3e3bc
vixplows <vicky@vickyplows.co.uk>
2017-07-23 21:56:56 +0100

    Pass Basket testItemListStartsEmpty

--- src/uk/co/vickyplows/shoppingbasket/Basket.java
+++ src/uk/co/vickyplows/shoppingbasket/Basket.java
@@ -6,4 +6,17 @@ public class Basket {
 
     private ArrayList<Items> itemList = new ArrayList<Items>();
     private Customer customer;
+
+    public Basket(Customer customer) {
+        this.customer = customer;
+    }
+
+
+    public int countItemsInItemList() {
+        return itemList.size();
+    }
+
+    public void addItemToList(Items item) {
+        itemList.add(item);
+    }
 }
--- tests/uk/co/vickyplows/shoppingbasket/BasketTest.java
+++ tests/uk/co/vickyplows/shoppingbasket/BasketTest.java
@@ -1,12 +1,42 @@
 package uk.co.vickyplows.shoppingbasket;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class BasketTest {
+
+    Basket basket;
+    Customer customer;
+    Milk milk;
+
     @Before
     public void setUp() throws Exception {
+        customer = new Customer("Louise", true);
+        milk = new Milk("a001", "Milk", 1.55, false);
+        basket = new Basket(customer);
+    }
+
+    @Test
+    public void testItemListStartsEmpty() {
+        int countItemList = basket.countItemsInItemList();
+        assertEquals(0, countItemList);
+    }
+
+    @Test
+    public void testCanAddItem() {
+        ArrayList<Items> itemList = new ArrayList<Items>();
+        basket.addItemToList(milk);
+        int countItemList = basket.countItemsInItemList();
+        assertEquals(1, countItemList);
+    }
+
+    @Test
+    public void testCanRemoveItem() {
+        
     }
 
 }
